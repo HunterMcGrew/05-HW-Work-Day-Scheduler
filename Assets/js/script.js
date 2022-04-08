8830705e0d956d9e013a734218152396f4ae3908
HunterLM <hunter109@gmail.com>
2022-04-07 22:55:05 -0500

    finished all but highlight functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,18 +9,21 @@ var currentTime = displayDay(currentTime);
 
 function displayDay() {
     // can i make it refresh seconds and minutes? 
-    var currentTime = moment().format('MMMM Do, YYYY, h:mm:ss a');
+    var currentTime = moment().format('MMMM Do, YYYY');
     timeEl.innerHTML = currentTime;
     // setInterval(() => {
     //     displayDay();
-    //       // should i call colorTime every minute?
+    //       // should i call colorTime every minute? doing it every minute overloads the browser
     // }, 1000);
     colorTime();
+    updateOnRefresh();
 };
+
+// function to create page?
 
 // change textarea based on time 
 function colorTime() {
-    rightNow = 11; //moment().format("H");
+    rightNow = 9; //moment().format("H");
     var time9 = document.querySelector("#time9");
     var time10 = document.querySelector("#time10");
     var time11 = document.querySelector("#time11");
@@ -31,12 +34,11 @@ function colorTime() {
     var time16 = document.querySelector("#time4");
     var time17 = document.querySelector("#time5");
 
-    
 
 
     // what about a while loop? didn't work
 
-    // what about a switch statement?
+    // what about a switch statement? that didn't work.
 
     // switch(rightNow) {
     //     case 9:
@@ -260,12 +262,12 @@ function colorTime() {
     // if (rightNow == 9) {
     //     time9.classList.add("present");
     //     time9.classList.remove("future", "past");
-    // }
-    // if (rightNow > 9) { 
+    // } 
+    // else if (rightNow > 9) { 
     //     time9.classList.add("past");
     //     time9.classList.remove("future", "present");
     // }
-    // if (rightNow < 9) {
+    // else if (rightNow < 9) {
     //     time9.classList.add("future");
     //     time9.classList.remove("present", "past");
     // }
@@ -358,5 +360,73 @@ function colorTime() {
 
 console.log(rightNow);
 
+// const saved = document.querySelector(".time-block");
+// const saveItems = document.querySelector(".saveBtn");
+// const items = [];
 
-// event deligation for the save stuff
+// var userInput = document.querySelector("#textInput");
+    function updateOnRefresh() {
+
+            var textSpot = document.querySelector("#textInput1");
+            var textSpot2 = document.querySelector("#textInput2");
+            var textSpot3 = document.querySelector("#textInput3");
+            var textSpot4 = document.querySelector("#textInput4");
+            var textSpot5 = document.querySelector("#textInput5");
+            var textSpot6 = document.querySelector("#textInput6");
+            var textSpot7 = document.querySelector("#textInput7");
+            var textSpot8 = document.querySelector("#textInput8");
+            var textSpot9 = document.querySelector("#textInput9");
+
+            var textOut1 = localStorage.getItem("text");
+            var textOut2 = localStorage.getItem("text2");
+            var textOut3 = localStorage.getItem("text3");
+            var textOut4 = localStorage.getItem("text4");
+            var textOut5 = localStorage.getItem("text5");
+            var textOut6 = localStorage.getItem("text6");
+            var textOut7 = localStorage.getItem("text7");
+            var textOut8 = localStorage.getItem("text8");
+            var textOut9 = localStorage.getItem("text9");
+        
+            textSpot.textContent = textOut1;
+            textSpot2.textContent = textOut2;
+            textSpot3.textContent = textOut3;
+            textSpot4.textContent = textOut4;
+            textSpot5.textContent = textOut5;
+            textSpot6.textContent = textOut6;
+            textSpot7.textContent = textOut7;
+            textSpot8.textContent = textOut8;
+            textSpot9.textContent = textOut9;
+
+    }
+
+
+var buttons = document.getElementsByClassName("saveBtn");
+    for (i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function(event) {
+            event.preventDefault();
+            
+            var text = document.querySelector("#textInput1").value;
+            var text2 = document.querySelector("#textInput2").value;
+            var text3 = document.querySelector("#textInput3").value;
+            var text4 = document.querySelector("#textInput4").value;
+            var text5 = document.querySelector("#textInput5").value;
+            var text6 = document.querySelector("#textInput6").value;
+            var text7 = document.querySelector("#textInput7").value;
+            var text8 = document.querySelector("#textInput8").value;
+            var text9 = document.querySelector("#textInput9").value;
+        // document.querySelector("textarea").innerHTML = text;
+
+        localStorage.setItem("text", text);
+        localStorage.setItem("text2", text2);
+        localStorage.setItem("text3", text3);
+        localStorage.setItem("text4", text4);
+        localStorage.setItem("text5", text5);
+        localStorage.setItem("text6", text6);
+        localStorage.setItem("text7", text7);
+        localStorage.setItem("text8", text8);
+        localStorage.setItem("text9", text9);
+
+        })
+    }
+
+    
